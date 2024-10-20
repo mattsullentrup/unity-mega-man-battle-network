9bcbac81261bb524ece1779b4e2c2d657b9c459c
mattsullentrup <msullentrup1@gmail.com>
2024-10-20 15:04:20 -0500

    Constrain player movement to cells

--- MegaManBattleNetwork/Assets/Battler/Player/Scripts/PlayerMovement.cs
+++ MegaManBattleNetwork/Assets/Battler/Player/Scripts/PlayerMovement.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
@@ -6,11 +7,22 @@ public class PlayerMovement : MonoBehaviour
 
     public void Move(Vector2Int direction)
     {
-        if (!Player.CanMove) return;
+        if (!Player.CanMove)
+            return;
+
+        var playerCell = Globals.WorldToCell2D(Player.transform.position);
+        if (!IsValidPosition(playerCell + direction))
+            return;
 
         var moveCommand = new MoveCommand(direction, Player);
         moveCommand.Execute();
         Player.CanMove = false;
         Player.StartMoveCooldown();
+    }
+
+    private bool IsValidPosition(Vector2Int cell)
+    {
+        var result = Player.ValidRows.Any(list => list.Contains(cell));
+        return result;
     }
 }
--- MegaManBattleNetwork/Assets/Utilities/Globals.cs
+++ MegaManBattleNetwork/Assets/Utilities/Globals.cs
@@ -3,4 +3,10 @@ using UnityEngine;
 public static class Globals
 {
     public const int CellSize = 32;
+    public static Vector2Int WorldToCell2D(Vector3 cell)
+    {
+        Vector2 pos = new(cell.x, cell.y);
+        var newPos = Vector2Int.RoundToInt(pos);
+        return newPos;
+    }
 }
