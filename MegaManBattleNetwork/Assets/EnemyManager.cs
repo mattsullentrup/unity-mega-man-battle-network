a59de457bfb7de94fda2e32ab64de6049c35b85b
mattsullentrup <msullentrup1@gmail.com>
2024-10-20 22:36:32 -0500

    Find all enemies on scene start

--- MegaManBattleNetwork/Assets/EnemyManager.cs
+++ MegaManBattleNetwork/Assets/EnemyManager.cs
@@ -1,11 +1,18 @@
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class EnemyManager : MonoBehaviour
 {
     private List<List<Vector2Int>> _allRows;
     private List<List<Vector2Int>> _enemyRows;
-    private List<Enemy> _enemies = new();
+    private List<Enemy> _enemies;
+
+    private void Start()
+    {
+        _enemies = FindObjectsByType<Enemy>(FindObjectsSortMode.InstanceID).ToList();
+        return;
+    }
 
     public void Initialize(List<List<Vector2Int>> allRows, List<List<Vector2Int>> enemyRows)
     {
