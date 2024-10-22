1a95d45741f8ab7294c1508c37b28a220eb588b3
mattsullentrup <msullentrup1@gmail.com>
2024-10-22 10:42:57 -0500

    Do more work on chip selection code

--- MegaManBattleNetwork/Assets/ChipSelection.cs
+++ MegaManBattleNetwork/Assets/ChipSelection.cs
@@ -18,10 +18,10 @@ public class ChipSelection : MonoBehaviour
     private const int _maxSelectedChips = 3;
     private const int _maxChipContainerSize = 10;
     private int _maxAvailableChips = _initialMaxChips;
-    // private List<ChipSO> _possibleChips;
     private List<ChipSO> _selectedChips = new();
     private List<GameObject> _dummyChips = new();
     private InputAction _secondaryAction;
+    private Dictionary<GameObject, ChipSO> _buttonData = new();
 
     private void OnEnable()
     {
@@ -78,17 +78,61 @@ public class ChipSelection : MonoBehaviour
 
     private void CreateNewChips()
     {
+        var existingChips = new List<ChipSO>();
+        foreach (Transform child in _chipContainer.transform)
+        {
+            Image image = child.gameObject.GetComponent("Image") as Image;
+            if (image == null)
+                continue;
 
+            if (_buttonData.TryGetValue(child.gameObject, out ChipSO chip))
+            {
+                existingChips.Add(chip);
+            }
+        }
+
+        var newChips = new List<ChipSO>();
+        while (existingChips.Count + newChips.Count < _maxAvailableChips)
+        {
+            int index = Random.Range(0, _possibleChips.Count);
+            var randomChip = _possibleChips[index];
+            newChips.Add(randomChip);
+        }
+
+        SetupNewRound(newChips);
     }
 
     private void SetupNewRound(List<ChipSO> newChips)
     {
+        foreach (Transform child in _chipContainer.transform)
+        {
+            Image image = child.gameObject.GetComponent("Image") as Image;
+            if (image == null && newChips.Count > 0)
+            {
+                var chip = newChips[0];
+                newChips.RemoveAt(0);
 
+                var chipUI = CreateNewChipUI(chip);
+                var index = child.GetSiblingIndex();
+                chipUI.transform.SetParent(_chipContainer.transform);
+                chipUI.transform.SetSiblingIndex(index + 1);
+                child.SetParent(null);
+                _dummyChips.Add(child.gameObject);
+            }
+
+            EventSystem.current.SetSelectedGameObject(_chipContainer.transform.GetChild(0).gameObject);
+            // _animationPlayer.Play("SlideChipSelectionContainer");
+            gameObject.SetActive(true);
+        }
     }
 
     private GameObject CreateNewChipUI(ChipSO chip)
     {
         var button = Instantiate(_chipButtonPrefab, new Vector3(0, 0, 0), Quaternion.identity);
+        Image image = button.GetComponent("Image") as Image;
+        _buttonData.Add(button, chip);
+        button.GetComponent<Button>().onClick.AddListener(SelectChip);
+        // chip_ui.focus_entered.connect(func(): _focused_chip_texture_rect.texture = chip.icon)
         return button;
     }
 
